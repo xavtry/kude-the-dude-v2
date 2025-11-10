c5bf559454bffcea3f59f5df439d28a09104a6ce
xavtry <sebastianishot68@gmail.com>
2025-11-09 20:32:35 -0500

    Update utils.js

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,1 +1,13 @@
+export function isUrlLike(text){
+try{
+if(/^\w+:\/\//.test(text)) return true;
+// quick domain check
+if(/^[^\s]+\.[a-z]{2,}$/i.test(text)) return true;
+}catch(e){}
+return false;
+}
 
+
+export function asSearchQuery(text){
+return 'https://www.google.com/search?q=' + encodeURIComponent(text);
+}
