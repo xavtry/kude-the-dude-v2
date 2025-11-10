d57a7407a561c5a8ca7557521fc2f84ed8d4a69d
xavtry <sebastianishot68@gmail.com>
2025-11-09 20:34:10 -0500

    Update server.js

--- proxy/server.js
+++ proxy/server.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const basicAuth = require('basic-auth');
+const cors = require('cors');
+const path = require('path');
 
+
+const app = express();
+const PORT = process.env.PORT || 3000;
